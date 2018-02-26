554baa7f2743b02d9dcbf6d679be999dc78d8444
Philip West <pwestdk@gmail.com>
2018-02-26 14:26:17 +0100

    test

--- test/Impl/HandlerImplTest.java
+++ test/Impl/HandlerImplTest.java
@@ -1,9 +1,7 @@
 package Impl;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
 
 class HandlerImplTest {
@@ -28,4 +26,50 @@ class HandlerImplTest {
         _actual = data.split(System.lineSeparator())[13]; // Magic number
         assertEquals(_expected, _actual);
 	}
+
+	@Test
+	void testGetCarData() {
+		
+	}
+
+	@Test
+	void testGetLongestParked() {
+		
+	}
+
+	@Test
+	void testSortCarsByNumberPlate() {
+		
+	}
+
+	@Test
+	void testSortByTime() {
+		
+	}
+
+	@Test
+	void testCalculateTicketPrice() {
+		
+	}
+
+	@Test
+	void testCalculateParkingtimeInDays() {
+		
+	}
+
+	@Test
+	void testGetIllegallyParkedCars() {
+		
+	}
+
+	@Test
+	void testGetReportedCars() {
+		
+	}
+
+	@Test
+	void testCalculateTotalTicketPrice() {
+		
+	}
+
 }
