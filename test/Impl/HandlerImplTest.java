a248452409b370c50094ac233002b2a97600ae73
Philip West <pwestdk@gmail.com>
2018-02-26 11:36:25 +0100

    test folder

---
+++ test/Impl/HandlerImplTest.java
@@ -0,0 +1,14 @@
+package Impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HandlerImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
